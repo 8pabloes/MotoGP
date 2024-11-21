ca55676b493861554f77a8fb29673a1a3fb0a23c
8pabloes <8pabloes@gmail.com>
2024-11-21 13:02:58 +0100

    Todo

---
+++ src/main/java/edu/pablo/Motogp1Application.java
@@ -0,0 +1,13 @@
+package edu.pablo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Motogp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Motogp1Application.class, args);
+	}
+
+}
